3fbede69d938cca9bfbc813801fc3d430fc01307
safa mejdoub <safaamejdoub@gmail.com>
2022-02-06 20:23:59 +0100

    formulaire boostrap

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+	@PostMapping("/result")
+	@ResponseBody
+	public String resultat(@RequestParam("nom") String n, @RequestParam("email") String e)
+	{
+		return "Vous avez taper un nom : " +n +" un email : "+e;
+	}
 	@RequestMapping("/info")  // ce qu'on tape ds l'url
 	public String info(Model model)
 	{
+		
 		String formation="fullstack";
 		ArrayList<String> names= new ArrayList<>();
 		names.add("OCP");
