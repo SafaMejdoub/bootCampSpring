5851bafeced4c296cd2231e755d65194914205b1
safa mejdoub <safaamejdoub@gmail.com>
2022-02-06 15:30:12 +0100

    passage data vers vue

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,14 +1,17 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/info")  // ce qu'on tape ds l'url
-	public String info()
+	public String info(Model model)
 	{
+		String formation="fullstack";
 		System.out.println("methode info");
+		model.addAttribute("workshop", formation);
 		return "home/info"; //schema fichier html templates/home
 	}
 	@RequestMapping("/affichage")
